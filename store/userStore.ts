baca4cd61df51eac93d43345d16cb3021656e80e
Saivenkat Mahendrakar <themsv@Saivenkats-MacBook-Pro.local>
2024-10-17 12:44:41 +0530

    sync userStore of zustand to AsyncStorage to persist

--- store/userStore.ts
+++ store/userStore.ts
@@ -1,13 +1,23 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 type User = {
   isLoggedIn: boolean;
   toggleLogin: () => void;
 };
 
-export const useUserStore = create<User>((set) => ({
-  isLoggedIn: false,
-  toggleLogin: () => {
-    set((state) => ({ ...state, isLoggedIn: !state.isLoggedIn }));
-  },
-}));
+export const useUserStore = create(
+  persist<User>(
+    (set) => ({
+      isLoggedIn: false,
+      toggleLogin: () => {
+        set((state) => ({ ...state, isLoggedIn: !state.isLoggedIn }));
+      },
+    }),
+    {
+      name: "taskly-user-store",
+      storage: createJSONStorage(() => AsyncStorage),
+    },
+  ),
+);
